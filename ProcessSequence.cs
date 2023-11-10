a460b8a65d9fb4054785437490ad59c87049f970
Nimble-Ste <ste@nimbleapproach.com>
2023-11-10 11:00:48 +0000

    quick demo to show how to add different object types to a list

---
+++ ProcessSequence.cs
@@ -0,0 +1,32 @@
+﻿namespace DifferentObjectsInList
+{
+
+    public class HangerSequence
+    {
+        public string HangerId { get; set; }
+
+        public List<ProcessSequence> ProcessSequences { get; set; }
+
+    }
+
+    public class ProcessSequence
+    {
+        public int Id { get; set; }
+
+        public int RobotProgramId { get; set; }
+
+        public int ProcessConveyor { get; set; }
+    }
+
+    public class PickAndProcess: ProcessSequence
+    {
+        public int RequiredDips { get; set; }
+
+        public int CurrentDips { get; set; }
+    }
+
+    public class ManualOffline : ProcessSequence
+    {
+        public bool ManualOfflineComplete { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace DifferentObjectsInList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hanger = new HangerSequence
+            {
+                HangerId = "HG123",
+                ProcessSequences = new List<ProcessSequence>
+                {
+                    new PickAndProcess
+                    {
+                        Id = 1, RobotProgramId = 20, ProcessConveyor = 2, CurrentDips = 0, RequiredDips = 4
+                    },
+                    new PickAndProcess
+                    {
+                        Id = 2, RobotProgramId = 10, ProcessConveyor = 3, CurrentDips = 0, RequiredDips = 1
+                    },
+                    new ManualOffline
+                    {
+                        Id = 3, RobotProgramId = 2, ProcessConveyor = 3, ManualOfflineComplete = false
+                    },
+                    new PickAndProcess
+                    {
+                        Id = 4, RobotProgramId = 10, ProcessConveyor = 3, CurrentDips = 0, RequiredDips = 2
+                    }
+                }
+            };
+
+            foreach (var sequence in hanger.ProcessSequences)
+            {
+                switch (sequence)
+                {
+                    case PickAndProcess pickAndProcess:
+                        Console.WriteLine($"PickAndProcess Sequence : {pickAndProcess.Id} {pickAndProcess.RobotProgramId} {pickAndProcess.ProcessConveyor} {pickAndProcess.CurrentDips} {pickAndProcess.RequiredDips}");
+                        break;
+                    case ManualOffline manualOffline:
+                        Console.WriteLine($"ManualOffline Sequence : {manualOffline.Id} {manualOffline.RobotProgramId} {manualOffline.ManualOfflineComplete}");
+                        break;
+                }
+            }
+        }
+    }
+}
